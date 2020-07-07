5235b2f3b9f73386221a13f79cd1a0b978290987
WWaken <1549200803@qq.com>
2020-07-07 18:28:04 +0800

    饿汉模式

---
+++ thread/src/ThreadDemo6/SingletonMain.java
@@ -0,0 +1,12 @@
+package ThreadDemo6;
+
+/**
+ * @ClassName: SingletonMain
+ * @Description:
+ * @Author: Ma Yuanyuan
+ */
+public class SingletonMain {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+    }
+}
